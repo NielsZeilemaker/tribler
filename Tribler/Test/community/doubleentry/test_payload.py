cbb23d2ef6f4988e23e4f35da87fd681d33d2c3e
Steffan Norberhuis <steffan@norberhuis.nl>
2014-11-26 15:56:30 +0100

    DoubleEntry: SignatureResponse

--- Tribler/Test/community/doubleentry/test_payload.py
+++ Tribler/Test/community/doubleentry/test_payload.py
@@ -3,7 +3,7 @@ __author__ = 'norberhuis'
 import unittest
 import time
 
-from Tribler.community.doubleentry.payload import RequestSignature
+from Tribler.community.doubleentry.payload import RequestSignature, SignatureResponse
 
 from Tribler.dispersy.crypto import ECCrypto
 
@@ -13,17 +13,32 @@ class TestPayload(unittest.TestCase):
     Test the payloads of the DoubleEntry community
     """
 
+    _SECURITY_LEVEL = u'high'
+
     def setUp(self):
         self.crypto = ECCrypto()
-        self.ec = self.crypto.generate_key(u'high')
+        self.ec = self.crypto.generate_key(self._SECURITY_LEVEL)
 
-    def test_RequestSignature_init(self):
+    def test_requestsignature_init(self):
         """
-        Test creation of initializing a RequestSignaturePayload.
-        :return:
+        Test initializing a RequestSignaturePayload.
         """
         # Act
         payload = RequestSignature.Implementation(self.ec)
-        # Arrange
+        # Assert
         self.assertAlmostEqual(time.time(), payload.timestamp, 0)
-        self.assertTrue(self.crypto.is_valid_signature(self.ec, repr(payload.timestamp), payload.signatureInitiator))
+        self.assertTrue(self.crypto.is_valid_signature(self.ec, repr(payload.timestamp), payload.signatureRequester))
+
+    def test_signaturerepsonse_init(self):
+        """
+        Test initializing a SignatureResponsePayload
+        """
+        # Arrange
+        # Create different key resembling the requester.
+        requester_ec = self.crypto.generate_key(self._SECURITY_LEVEL)
+        request = RequestSignature.Implementation(requester_ec)
+        # Act
+        payload = SignatureResponse.Implementation(self.ec, request)
+        # Assert
+        data = repr(request.timestamp) + request.signatureRequester
+        self.assertTrue(self.crypto.is_valid_signature(self.ec, data, payload.signatureResponder))
--- Tribler/community/doubleentry/payload.py
+++ Tribler/community/doubleentry/payload.py
@@ -6,7 +6,6 @@ from Tribler.dispersy.payload import Payload
 from Tribler.dispersy.crypto import ECCrypto
 
 
-
 class RequestSignature(Payload):
     """
     Payload for message to Request Signing a TimeStamp.
@@ -27,12 +26,32 @@ class RequestSignature(Payload):
 
         def _sign_timestamp(self, ec):
             """
-            Signs the timestamp and saves the signature in signatureInitiator.
+            Signs the timestamp and saves the signature in signatureRequester.
             :param ec: Elliptic Curve Key Object to be used to sign the timestamp.
             :return:
             """
-
             crypto = ECCrypto()
             # Convert the timestamp to str.
             data = repr(self.timestamp)
-            self.signatureInitiator = crypto.create_signature(ec, data)
+            self.signatureRequester = crypto.create_signature(ec, data)
+
+
+class SignatureResponse(Payload):
+    """
+    Payload for message that will respond to a Signature Request containing
+    the Signature of {timestamp,signature_requester}.
+    """
+
+    class Implementation(Payload.Implementation):
+
+        def __init__(self, ec, request):
+            """
+            Creates the payload containing the signature of {timestamp,signature_requester}
+            by the recipient of the SignatureRequest.
+            :param ec: Elliptic Curve Key Object to be used to sign the timestamp
+            :param request: original SignatureRequest.
+            """
+            crypto = ECCrypto()
+            # Convert the timestamp to str and concatenate the requester signature
+            data = repr(request.timestamp) + request.signatureRequester
+            self.signatureResponder = crypto.create_signature(ec, data)
