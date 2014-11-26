fa8de084d6767a7cc7ebe5ee5f93a7070aac6e4b
Steffan Norberhuis <steffan@norberhuis.nl>
2014-11-26 15:15:27 +0100

    DoubleEntry: Payload:  * Added comments  * Renamed test_init to test_RequestSignature_init

--- Tribler/Test/community/doubleentry/test_payload.py
+++ Tribler/Test/community/doubleentry/test_payload.py
@@ -9,12 +9,19 @@ from Tribler.dispersy.crypto import ECCrypto
 
 
 class TestPayload(unittest.TestCase):
+    """
+    Test the payloads of the DoubleEntry community
+    """
 
     def setUp(self):
         self.crypto = ECCrypto()
         self.ec = self.crypto.generate_key(u'high')
 
-    def test_init(self):
+    def test_RequestSignature_init(self):
+        """
+        Test creation of initializing a RequestSignaturePayload.
+        :return:
+        """
         # Act
         payload = RequestSignature.Implementation(self.ec)
         # Arrange
--- Tribler/community/doubleentry/payload.py
+++ Tribler/community/doubleentry/payload.py
@@ -6,11 +6,19 @@ from Tribler.dispersy.payload import Payload
 from Tribler.dispersy.crypto import ECCrypto
 
 
+
 class RequestSignature(Payload):
+    """
+    Payload for message to Request Signing a TimeStamp.
+    """
 
     class Implementation(Payload.Implementation):
 
         def __init__(self, ec):
+            """
+            Creates the payload taking the timestamp from the system clock.
+            :param ec: Elliptic Curve Key Object to be used to sign the timestamp
+            """
             # Get the current timestamp.
             self.timestamp = time.time()
 
@@ -18,6 +26,12 @@ class RequestSignature(Payload):
             self._sign_timestamp(ec)
 
         def _sign_timestamp(self, ec):
+            """
+            Signs the timestamp and saves the signature in signatureInitiator.
+            :param ec: Elliptic Curve Key Object to be used to sign the timestamp.
+            :return:
+            """
+
             crypto = ECCrypto()
             # Convert the timestamp to str.
             data = repr(self.timestamp)
