4483cec249b32912d79ffd746bd21f434d2909dd
Steffan Norberhuis <steffan@norberhuis.nl>
2015-04-14 14:42:51 +0200

    DoubleEntry: improved graph layout.

--- Tribler/community/doubleentry/graph.py
+++ Tribler/community/doubleentry/graph.py
@@ -4,6 +4,8 @@ Functionality to draw graphs for the double entry chain.
 import networkx as nx
 import matplotlib.pyplot as plot
 
+from Tribler.community.doubleentry.database import encode_db
+
 from Tribler.community.doubleentry.database import GENESIS_ID
 
 
@@ -19,12 +21,14 @@ class GraphDrawer:
         # Get all keys
         keys = self._persistence.get_ids()
         # Every key is a node and iterate over each node
+        # Encoding is needed for certain networkx layouts to work.
         for key in keys:
-            self.graph.add_node(key)
+            encoded_key = encode_db(key)
+            self.graph.add_node(encoded_key)
             # Add the edges
             block = self._persistence.get(key)
-            self._add_edge(key, block.previous_hash_requester, block.public_key_requester)
-            self._add_edge(key, block.previous_hash_responder, block.public_key_responder)
+            self._add_edge(encoded_key, encode_db(block.previous_hash_requester), encode_db(block.public_key_requester))
+            self._add_edge(encoded_key, encode_db(block.previous_hash_responder), encode_db(block.public_key_responder))
 
     def _add_edge(self, head, tail, unique_identifier):
         """
@@ -35,11 +39,14 @@ class GraphDrawer:
         :param unique_identifier: Unique identifier to be added to hash.
         :return:
         """
-        if tail == GENESIS_ID:
+        if tail == encode_db(GENESIS_ID):
             self.graph.add_edge(head, tail + unique_identifier)
         else:
             self.graph.add_edge(head, tail)
 
     def draw_graph(self):
-        nx.draw(self.graph)
+        pos = nx.graphviz_layout(self.graph, prog='neato')
+
+        nx.draw(self.graph, pos)
+
         plot.show(self.graph)
